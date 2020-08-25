11bc35643b2c208bef36f488d1fa42a1b8a989c2
Isaac Vianna <isaacsvianna@gmail.com>
2020-08-25 16:57:57 -0300

    até o file loader

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
+
+import './App.css';
+import casal from './assets/casal.jpg';
+
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desemvolvimento de app', 'Front-End Web']);
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-End Web']);
 
     function handleAddProject() {
-        projects.push(`Novo Projeto ${Date.now()}`);
+        // projects.push(`Novo Projeto ${Date.now()}`);
+
+        setProjects([...projects,`Novo Projeto ${Date.now()}`]);
 
         console.log(projects);
     }
 
     return (
         <>
+            
             <Header title="homepage" />
+            
+            <img width={400} src={casal} />
+
+            
+
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
             </ul>
