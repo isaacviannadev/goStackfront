2588fd0820b4ae58d590a75dc6a3863de2a99f59
Isaac Vianna <isaacsvianna@gmail.com>
2020-08-26 20:10:44 -0300

    Conectando com back fiinal de modulo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desemvolvimento de app', 'Front-End Web']);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject() {
-        projects.push(`Novo Projeto ${Date.now()}`);
+    useEffect(()=>{
+       api.get('/projects').then(response => {
+        setProjects(response.data);
+       }); 
+    }, []);
 
-        console.log(projects);
+    async function handleAddProject() {
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+     const response = await api.post('/projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: 'Isaac Vianna'
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
         <>
             <Header title="homepage" />
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
-            <button type="button" onClick={handleAddProject} >Adicionar Projeto</button>
+            <button type="button" onClick={handleAddProject}> Adicionar Projeto </button>
         </>
     );
 }
